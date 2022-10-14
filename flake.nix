27ebddb9288311e4661ae180f7920b231dd56f9a
Adam Stephens <adam@valkor.net>
2022-10-14 22:51:26 +0000

    dead code cleanup

--- flake.nix
+++ flake.nix
@@ -23,19 +23,7 @@
         system,
         self',
         ...
-      }:
-      # self.lib.genWithNixpkgs {
-      #   inherit self' system;
-      #   pkgs = inputs.nixpkgs-unstable.legacyPackages.${system};
-      #   release = "unstable";
-      # }
-      # // self.lib.genWithNixpkgs {
-      #   inherit self' system;
-      #   pkgs = inputs.nixpkgs-2205.legacyPackages.${system};
-      #   release = "22.05";
-      # }
-      # //
-      {
+      }: {
         _module.args.pkgs = inputs.nixpkgs-2205.legacyPackages.${system};
 
         devShells.default = pkgs.mkShellNoCC {
@@ -48,98 +36,9 @@
       };
     }
     // {
-      # lib.genWithNixpkgs = {
-      #   pkgs,
-      #   release,
-      #   system,
-      #   self',
-      # }: let
-      #   releaseString = "nixpkgs-${release}";
-      #   releaseVM = "${releaseString}-image-vm";
-      #   releaseContainer = "${releaseString}-image-container";
-      #   importerVM = "import-${releaseString}-image-vm";
-      #   importerContainer = "import-${releaseString}-image-container";
-      # in {
-      #   _module.args.pkgs = pkgs;
-
-      #   checks.${releaseContainer} = import ./nixos-test.nix {
-      #     inherit pkgs;
-      #     makeTest = import (pkgs.path + "/nixos/tests/make-test-python.nix");
-
-      #     testName = "lxd-container-test";
-      #     importer = self'.packages.${importerContainer};
-      #     release = self.nixosConfigurations.image-container.config.system.nixos.release;
-      #   };
-
-      #   checks.${releaseVM} = import ./nixos-test.nix {
-      #     inherit pkgs;
-      #     makeTest = import (pkgs.path + "/nixos/tests/make-test-python.nix");
-
-      #     testName = "lxd-vm-test";
-      #     importer = self'.packages.${importerVM};
-      #     release = self.nixosConfigurations.image-vm.config.system.nixos.release;
-      #     vm = true;
-      #   };
-
-      #   packages = rec {
-      #     ${releaseVM} = pkgs.symlinkJoin {
-      #       name = "${releaseVM}";
-      #       paths = [
-      #         self.nixosConfigurations.image-vm.config.system.build.qemuImage
-      #         self.nixosConfigurations.image-vm.config.system.build.metadata
-      #       ];
-      #     };
-      #     ${importerVM} = pkgs.writeScriptBin "import-image" ''
-      #       IMAGE="${releaseVM}"
-
-      #       echo "Importing VM image $IMAGE"
-      #       lxc image import --alias nixos/${self.nixosConfigurations.${releaseVM}.config.system.nixos.release} \
-      #         $IMAGE/tarball/nixos-lxd-metadata-${system}.tar.xz \
-      #         $IMAGE/nixos.qcow2
-      #     '';
-
-      #     ${releaseContainer} = pkgs.symlinkJoin {
-      #       name = "${releaseContainer}";
-      #       paths = [
-      #         self.nixosConfigurations.image-container.config.system.build.tarball
-      #         self.nixosConfigurations.image-container.config.system.build.metadata
-      #       ];
-      #     };
-      #     ${importerContainer} = pkgs.writeScriptBin "import-image" ''
-      #       IMAGE="${releaseContainer}"
-
-      #       echo "Importing container image $IMAGE"
-      #       lxc image import --alias nixos/${self.nixosConfigurations.image-container.config.system.nixos.release} \
-      #         $IMAGE/tarball/nixos-lxd-metadata-${system}.tar.xz \
-      #         $IMAGE/tarball/nixos-lxd-image-${system}.tar.xz
-      #     '';
-      #   };
-      # };
-
       nixosModules.agent = import ./modules/agent.nix;
       nixosModules.container = import ./modules/container.nix;
       nixosModules.imageMetadata = import ./modules/image-metadata.nix;
       nixosModules.vm = import ./modules/vm.nix;
-
-      # nixosConfigurations.image-vm = inputs.nixpkgs-unstable.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   modules = [
-      #     self.nixosModules.vm
-      #     self.nixosModules.image-metadata
-      #     {
-      #       system.stateVersion = "22.05";
-      #     }
-      #   ];
-      # };
-      # nixosConfigurations.image-container = inputs.nixpkgs-unstable.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   modules = [
-      #     self.nixosModules.container
-      #     self.nixosModules.image-metadata
-      #     {
-      #       system.stateVersion = "22.05";
-      #     }
-      #   ];
-      # };
     };
 }
