034c32ef993495ec4e0fd30767bf79f35c5ea3be
Adam Stephens <adam@valkor.net>
2023-03-28 16:22:29 -0400

    ovmf: update from lxd 5.12, add aarch64 support

--- packages/ovmf/default.nix
+++ packages/ovmf/default.nix
@@ -1,15 +1,24 @@
 {stdenv, ...}:
 stdenv.mkDerivation {
   pname = "ovmf";
-  version = "0.1.0-lxd5.7";
+  version = "0.1.1-lxd5.12";
   src = ./.;
   dontBuild = true;
 
   outputs = ["out" "fd"];
 
-  installPhase = ''
-    mkdir $out
-    mkdir -p $fd/FV
-    cp OVMF* $fd/FV/
-  '';
+  installPhase =
+    ''
+      mkdir $out
+      mkdir -p $fd/FV
+    ''
+    + (
+      if stdenv.hostPlatform.isx86
+      then ''
+        cp x86_64/OVMF* $fd/FV/
+      ''
+      else ''
+        cp aarch64/OVMF* $fd/FV/
+      ''
+    );
 }
