20cd72e1ccb3ea50aa65ca6404828541d7f5c324
Adam Stephens <adam@valkor.net>
2023-07-03 12:23:48 -0400

    lxd: ovmf in wrapper doesn't need to be a linkfarm

--- packages/lxd/wrapper.nix
+++ packages/lxd/wrapper.nix
@@ -65,23 +65,6 @@
       exec '${apparmor-parser}/bin/apparmor_parser' -I '${apparmor-profiles}/etc/apparmor.d' "$@"
     '')
   ];
-
-  firmware = linkFarm "lxd-firmware" [
-    {
-      name = "share/OVMF/OVMF_CODE.fd";
-      path = "${OVMFFull.fd}/FV/OVMF_CODE.fd";
-    }
-    {
-      name = "share/OVMF/OVMF_VARS.fd";
-      path = "${OVMFFull.fd}/FV/OVMF_VARS.fd";
-    }
-    {
-      name = "share/OVMF/OVMF_VARS.ms.fd";
-      path = "${OVMFFull.fd}/FV/OVMF_VARS.fd";
-    }
-  ];
-
-  LXD_OVMF_PATH = "${firmware}/share/OVMF";
 in
   symlinkJoin {
     name = "lxd-${lxd.version}";
@@ -90,7 +73,7 @@ in
 
     nativeBuildInputs = [makeWrapper];
     postBuild = ''
-      wrapProgram $out/bin/lxd --prefix PATH : ${lib.makeBinPath binPath}:${qemu_kvm}/libexec:${zfs}/lib/udev:$out/bin --set LXD_OVMF_PATH ${LXD_OVMF_PATH}
+      wrapProgram $out/bin/lxd --prefix PATH : ${lib.makeBinPath binPath}:${qemu_kvm}/libexec:${zfs}/lib/udev:$out/bin --set LXD_OVMF_PATH ${OVMFFull.fd}/FV
     '';
 
     passthru.tests = {
